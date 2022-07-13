e8445fd438af13decf37694645b10bf5195ae6ad
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-07-12 22:01:25 -0300

    add updatesliderinput

--- 10_Dynamic_UI/02_simple_uses.R
+++ 10_Dynamic_UI/02_simple_uses.R
@@ -10,14 +10,24 @@
 library(shiny)
 
 ui <- fluidPage(
+    
     sliderInput(inputId = "x1", label = "x1", value = 0, min = -10, max = 10),
     sliderInput(inputId = "x2", label = "x2", value = 0, min = -10, max = 10),
     sliderInput(inputId = "x3", label = "x1", value = 0, min = -10, max = 10),
     actionButton(inputId = "reset", label = "Resetar")
+    
 )
 
 server <- function(input, output, session) {
   
+    observeEvent(input$reset, {
+        
+        updateSliderInput(inputId = "x1", value = 0)
+        updateSliderInput(inputId = "x2", value = 0)
+        updateSliderInput(inputId = "x3", value = 0)
+        
+    })
+    
 }
 
 shinyApp(ui, server)
