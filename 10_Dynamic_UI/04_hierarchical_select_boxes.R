8b24ae3c8a5c6a9023492bc915541e40b5c07107
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-07-15 23:27:26 -0300

    building ui

--- 10_Dynamic_UI/04_hierarchical_select_boxes.R
+++ 10_Dynamic_UI/04_hierarchical_select_boxes.R
@@ -7,19 +7,62 @@
 # Objective: allow interactive drill down across multiple categories
 
 
+# Load packages -----------------------------------------------------------
+library(shiny)
+
+
 # Get data ----------------------------------------------------------------
 
 # read data
 sales <- vroom::vroom(
-    file = "10_Dynamic_UI/sales_data_sample.csv",
+    file = "../10_Dynamic_UI/sales_data_sample.csv",
     col_types = list(),
     na = ""
     ) |> janitor::clean_names()
 
 # see structure
-sales |> dplyr::glimpse()
+# sales |> dplyr::glimpse()
+
 
 # arrange data
 sales |> 
     dplyr::select(territory, customername, ordernumber, everything()) |> 
     dplyr::arrange(ordernumber)
+
+
+# The logic of the data follow a natural hierarchy in:
+# Each territory contains customers.
+# Each customer has multiple orders.
+# Each order contains rows.
+
+# The ideia is to create a user interface where we can:
+# Select a territory to see all customers.
+# Select a customer to see all orders.
+# Select an order to see the underlying rows
+
+
+ui <- fluidPage(
+    selectizeInput(
+        inputId = "territory",
+        label = "Territory",
+        choices = unique(sales$territory)
+    ),
+    
+    selectInput(
+        inputId = "customername",
+        label = "Customer",
+        choices = NULL # will be dynamically generated
+    ),
+    
+    selectInput(
+        inputId = "ordernumber",
+        label = "Order number",
+        choices = NULL # will be dynamically generated
+    )
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
