3c7288ae56436c233fbf59ffa0dca4d1765ba1ba
Maykon G. Pedro <maykonglaffite@gmail.com>
2022-07-17 20:22:50 -0300

    add second update input and the table to show data

--- 10_Dynamic_UI/04_hierarchical_select_boxes.R
+++ 10_Dynamic_UI/04_hierarchical_select_boxes.R
@@ -58,13 +58,15 @@ ui <- fluidPage(
         inputId = "ordernumber",
         label = "Order number",
         choices = NULL # will be dynamically generated
-    )
+    ),
+    
+    tableOutput(outputId = "preview_data")
 )
 
 server <- function(input, output, session) {
   
   
-  # create a reactive base with the territory filtered
+  # create a reactive base with the territory filtered ------------------------
   territory_data <- reactive({
     sales |> dplyr::filter(territory == input$territory)
   })
@@ -72,48 +74,60 @@ server <- function(input, output, session) {
   # first update - using territory filtered data to update customer name
   observeEvent(territory_data(), {
     
+    # generate new choices
     new_choices <- unique(territory_data()$customername)
     
+    # tidy way
+    # new_choices <- territory_data() |>
+    #   dplyr::distinct(customername) |>
+    #   dplyr::pull(customername)
+    
+    # update input
     updateSelectInput(
       inputId = "customername",
       choices = new_choices
     )
     
-    
   })
+  
+  
+  # create a reactive base with the customername filtered ---------------------
+  customer_data <- reactive({
+    req(input$customername)
+    territory_data() |> dplyr::filter(customername == input$customername)
+  })
+  
+  # second update - using customername filtered data to update ordernumbers
+  observeEvent(customer_data(), {
+    
+    # generate new choices
+    new_choices <- unique(customer_data()$ordernumber)
+    
+    # update input
+    updateSelectInput(
+      inputId = "ordernumber",
+      choices = new_choices
+    )
+  })
+  
+  
+  # output to show data -------------------------------------------------------
+  output$preview_data <- renderTable({
+    
+    # require order number input
+    req(input$ordernumber)
+    
+    # filter data and select some columns
+    customer_data() |> 
+      dplyr::filter(ordernumber == input$ordernumber) |> 
+      dplyr::select(
+        quantityordered,
+        priceeach,
+        productcode
+      )
+  })
+  
 }
 
 shinyApp(ui, server)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
